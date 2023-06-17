1633666d4c7a21d0d7ead695824a80fcd4f1af0f
internessi <50959344+internessi@users.noreply.github.com>
2023-06-17 14:55:07 +0200

    Webserver

--- AlphaTrafo.ino
+++ AlphaTrafo.ino
@@ -2,10 +2,10 @@
 // the code is documentation enough !
 
 bool DS18B20 = true;
-bool CAPTIVE = false;
+bool WEBSERVER = true;
 bool SHT11 = true;
-bool MEASURE = true;
-bool GOSLEEP = true;
+bool MEASURE = false;
+bool GOSLEEP = false;
 bool actionSetup = false;
 bool actionHeat = false;
 bool actionReg = false;
@@ -33,15 +33,77 @@ esp_sleep_wakeup_cause_t wakeup_reason;
 #include "AlphaSHT11.h"                     // #include <SHT1x-ESP.h>
 #include "AlphaDS18B20.h"                   // #include <OneWire.h>, <DallasTemperature.h>
 
+
 void setup(){
                     
   relaisOFF();                              // SUB - better be safe than sorry
   setupGPIO();                              // SUB - setup LED, RLY  
   Serial.begin(115200);                     // setup serial at 115200 
+
+  // if your web page or XML are large, you may not get a call back from the web page
+  // and the ESP will think something has locked up and reboot the ESP
+  // not sure I like this feature, actually I kinda hate it
+  // disable watch dog timer 0
+  disableCore0WDT();
+
+  // maybe disable watch dog timer 1 if needed
+  //  disableCore1WDT();
+
+  // just an update to progress
+  Serial.println("starting server");
+
+  // if you have this #define USE_INTRANET,  you will connect to your home intranet, again makes debugging easier
+#ifdef USE_INTRANET
+  WiFi.begin(LOCAL_SSID, LOCAL_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("IP address: "); Serial.println(WiFi.localIP());
+  Actual_IP = WiFi.localIP();
+#endif
+
+  // if you don't have #define USE_INTRANET, here's where you will creat and access point
+  // an intranet with no internet connection. But Clients can connect to your intranet and see
+  // the web page you are about to serve up
+#ifndef USE_INTRANET
+  WiFi.softAP(AP_SSID, AP_PASS);
+  delay(100);
+  WiFi.softAPConfig(PageIP, gateway, subnet);
+  delay(100);
+  Actual_IP = WiFi.softAPIP();
+  Serial.print("IP address: "); Serial.println(Actual_IP);
+#endif
+
+  printWifiStatus();
+
+
+  // these calls will handle data coming back from your web page
+  // this one is a page request, upon ESP getting / string the web page will be sent
+  server.on("/", SendWebsite);
+
+  // upon esp getting /XML string, ESP will build and send the XML, this is how we refresh
+  // just parts of the web page
+  server.on("/xml", SendXML);
+
+  // upon ESP getting /UPDATE_SLIDER string, ESP will execute the UpdateSlider function
+  // same notion for the following .on calls
+  // add as many as you need to process incoming strings from your web page
+  // as you can imagine you will need to code some javascript in your web page to send such strings
+  // this process will be documented in the SuperMon.h web page code
+  server.on("/UPDATE_SLIDER", UpdateSlider);
+  server.on("/BUTTON_0", ProcessButton_0);
+  server.on("/BUTTON_1", ProcessButton_1);
+
+  // finally begin the server
+  server.begin();
+
+
+  delay(1000); LEDredgreenfast();  
   Serial.println("- - - - -");              // enter setup
-  setModemSleep();                          // SUB - turn off WIFI/BT set CPU to 40 Mhz
-  EEPROM.begin(32);
   
+  // setModemSleep();                          // SUB - turn off WIFI/BT set CPU to 40 Mhz
+  EEPROM.begin(32);
   wakeup_reason = esp_sleep_get_wakeup_cause(); // preperation for deep sleep
   if (wakeup_reason != ESP_SLEEP_WAKEUP_TIMER)  // skipping init of sensors
   {
@@ -57,15 +119,35 @@ void setup(){
     calcDP();                               // calculate drew point
     delay(1000); LEDredgreenfast(); 
     Serial.println("TP: " + String(dp) + "ºC"); 
-    setupCAPTIVE();                         // WIFI - setup captive
+ //   setupCAPTIVE();                         // WIFI - setup captive
     delay(1000); LEDredgreenfast(); 
     Serial.println("- - - - -");            // exit setup
   }
 }
 
 void loop(){
-  loopCAPTIVE();                            // active captive        
+
+  if (WEBSERVER) {
+    if ((millis() - SensorUpdate) >= 10000) {
+      //Serial.println("Reading Sensors");
+      SensorUpdate = millis();
+      LEDredgreen();
+      readSHT11();                            // read SHT11
+      Serial.println("HUM Luft : " + String(humSHT11) + "%"); 
+      Serial.println("TMP Luft : " + String(tmpSHT11) + "ºC");
+      readDS18B20();                          // read DS18B20
+      Serial.println("TMP Wand: " + String(tmpDS18B20) + "ºC");
+      calcDP();                               // calculate drew point
+      Serial.println("TP: " + String(dp) + "ºC"); 
+    }
+  }
+  // no matter what you must call this handleClient repeatidly--otherwise the web page
+  // will not get instructions to do something
+  server.handleClient();
+
+
   
+ // loopCAPTIVE();                            // active captive        
   if (MEASURE) {
     LEDredgreen();
     readSHT11();                            // read SHT11
--- AlphaWifi.h
+++ AlphaWifi.h
@@ -1,111 +1,237 @@
 // AlphaTrafo by Frank Wolf - 06/2023 
 // the code is documentation enough !
 
-#include <DNSServer.h>
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebSrv.h>
 
-DNSServer dnsServer;
-AsyncWebServer server(80);
-String user_name;
-String proficiency;
-bool name_received = false;
-bool proficiency_received = false;
+#include <WiFi.h>       // standard library
+#include <WebServer.h>  // standard library
+#include "AlphaHtml.h"                       // .h file that stores your html page code
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>Captive Portal Demo</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head><body>
-  <h3>Captive Portal Demo</h3>
-  <br><br>
-  <form action="/get">
-    <br>
-    Name: <input type="text" name="name">
-    <br>
-    ESP32 Proficiency: 
-    <select name = "proficiency">
-      <option value=Beginner>Beginner</option>
-      <option value=Advanced>Advanced</option>
-      <option value=Pro>Pro</option>
-    </select>
-    <input type="submit" value="Submit">
-  </form>
-</body></html>)rawliteral";
+// here you post web pages to your homes intranet which will make page debugging easier
+// as you just need to refresh the browser as opposed to reconnection to the web server
+#define USE_INTRANET
+
+// replace this with your homes intranet connect parameters
+#define LOCAL_SSID "ALARM4U"
+#define LOCAL_PASS "Wolf1212"
+
+// once  you are read to go live these settings are what you client will connect to
+#define AP_SSID "TestWebSite"
+#define AP_PASS "023456789"
+
+// start your defines for pins for sensors, outputs etc.
+#define PIN_OUTPUT 26 // connected to nothing but an example of a digital write from the web page
+#define PIN_FAN 27    // pin 27 and is a PWM signal to control a fan speed
+#define PIN_LED 2     //On board LED
+#define PIN_A0 34     // some analog input sensor
+#define PIN_A1 35     // some analog input sensor
+
+// variables to store measure data and sensor states
+int BitsA0 = 0, BitsA1 = 0;
+float VoltsA0 = 0, VoltsA1 = 0;
+int FanSpeed = 0;
+bool LED0 = false, SomeOutput = false;
+uint32_t SensorUpdate = 0;
+int FanRPM = 0;
+
+// the XML array size needs to be bigger that your maximum expected size. 2048 is way too big for this example
+char XML[1024];
+
+// just some buffer holder for char operations
+char buf[32];
+
+// variable for the IP reported when you connect to your homes intranet (during debug mode)
+IPAddress Actual_IP;
+
+// definitions of your desired intranet created by the ESP32
+IPAddress PageIP(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress ip;
+
+// gotta create a server
+WebServer server(80);
 
 
-class CaptiveRequestHandler : public AsyncWebHandler {
-public:
-  CaptiveRequestHandler() {}
-  virtual ~CaptiveRequestHandler() {}
+// function managed by an .on method to handle slider actions on the web page
+// this example will get the passed string called VALUE and conver to a pwm value
+// and control the fan speed
+void UpdateSlider() {
 
-  bool canHandle(AsyncWebServerRequest *request){
-    //request->addInterestingHeader("ANY");
-    return true;
-  }
+  // many I hate strings, but wifi lib uses them...
+  String t_state = server.arg("VALUE");
 
-  void handleRequest(AsyncWebServerRequest *request) {
-    request->send_P(200, "text/html", index_html); 
-  }
-};
+  // conver the string sent from the web page to an int
+  FanSpeed = t_state.toInt();
+  Serial.print("UpdateSlider"); Serial.println(FanSpeed);
+  // now set the PWM duty cycle
+  // ledcWrite(0, FanSpeed);
 
-void setupServer(){
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/html", index_html); 
-      Serial.println("Client Connected");
-  });
-    
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-      String inputMessage;
-      String inputParam;
-  
-      if (request->hasParam("name")) {
-        inputMessage = request->getParam("name")->value();
-        inputParam = "name";
-        user_name = inputMessage;
-        Serial.println(inputMessage);
-        name_received = true;
-      }
 
-      if (request->hasParam("proficiency")) {
-        inputMessage = request->getParam("proficiency")->value();
-        inputParam = "proficiency";
-        proficiency = inputMessage;
-        Serial.println(inputMessage);
-        proficiency_received = true;
-      }
-      request->send(200, "text/html", "The values entered by you have been successfully sent to the device <br><a href=\"/\">Return to Home Page</a>");
-  });
+  // YOU MUST SEND SOMETHING BACK TO THE WEB PAGE--BASICALLY TO KEEP IT LIVE
+
+  // option 1: send no information back, but at least keep the page live
+  // just send nothing back
+  // server.send(200, "text/plain", ""); //Send web page
+
+  // option 2: send something back immediately, maybe a pass/fail indication, maybe a measured value
+  // here is how you send data back immediately and NOT through the general XML page update code
+  // my simple example guesses at fan speed--ideally measure it and send back real data
+  // i avoid strings at all caost, hence all the code to start with "" in the buffer and build a
+  // simple piece of data
+  FanRPM = map(FanSpeed, 0, 255, 0, 2400);
+  strcpy(buf, "");
+  sprintf(buf, "%d", FanRPM);
+  sprintf(buf, buf);
+
+  // now send it back
+  server.send(200, "text/plain", buf); //Send web page
+
 }
 
-void setupCAPTIVE(){
-  if(CAPTIVE){
-    delay(1000);
-    Serial.println();
-    Serial.println("Setting up AP Mode");
-    WiFi.mode(WIFI_AP); 
-    WiFi.softAP("esp-captive");
-    Serial.print("AP IP address: ");Serial.println(WiFi.softAPIP());
-    Serial.println("Setting up Async WebServer");
-    setupServer();
-    Serial.println("Starting DNS Server");
-    dnsServer.start(53, "*", WiFi.softAPIP());
-    server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
-    //more handlers...
-    server.begin();
-    }// end CAPTIVE
-  }
+// now process button_0 press from the web site. Typical applications are the used on the web client can
+// turn on / off a light, a fan, disable something etc
+void ProcessButton_0() {
 
-void loopCAPTIVE(){
-   if(CAPTIVE){  
-  dnsServer.processNextRequest();
-  if(name_received && proficiency_received){
-      Serial.print("Hello ");Serial.println(user_name);
-      Serial.print("You have stated your proficiency to be ");Serial.println(proficiency);
-      name_received = false;
-      proficiency_received = false;
-      Serial.println("We'll wait for the next client now");
+  //
+
+
+  LED0 = !LED0;
+  digitalWrite(PIN_LED, LED0);
+  Serial.print("Button 0 "); Serial.println(LED0);
+  // regardless if you want to send stuff back to client or not
+  // you must have the send line--as it keeps the page running
+  // if you don't want feedback from the MCU--or let the XML manage
+  // sending feeback
+
+  // option 1 -- keep page live but dont send any thing
+  // here i don't need to send and immediate status, any status
+  // like the illumination status will be send in the main XML page update
+  // code
+  server.send(200, "text/plain", ""); //Send web page
+
+  // option 2 -- keep page live AND send a status
+  // if you want to send feed back immediataly
+  // note you must have reading code in the java script
+  /*
+    if (LED0) {
+    server.send(200, "text/plain", "1"); //Send web page
     }
-  }// end CAPTIVE
+    else {
+    server.send(200, "text/plain", "0"); //Send web page
+    }
+  */
+
 }
+
+// same notion for processing button_1
+void ProcessButton_1() {
+
+  // just a simple way to toggle a LED on/off. Much better ways to do this
+  Serial.println("Button 1 press");
+  SomeOutput = !SomeOutput;
+
+  digitalWrite(PIN_OUTPUT, SomeOutput);
+Serial.print("Button 1 "); Serial.println(LED0);
+  // regardless if you want to send stuff back to client or not
+  // you must have the send line--as it keeps the page running
+  // if you don't want feedback from the MCU--or send all data via XML use this method
+  // sending feeback
+  server.send(200, "text/plain", ""); //Send web page
+
+  // if you want to send feed back immediataly
+  // note you must have proper code in the java script to read this data stream
+  /*
+    if (some_process) {
+    server.send(200, "text/plain", "SUCCESS"); //Send web page
+    }
+    else {
+    server.send(200, "text/plain", "FAIL"); //Send web page
+    }
+  */
+}
+
+
+// code to send the main web page
+// PAGE_MAIN is a large char defined in SuperMon.h
+void SendWebsite() {
+
+  Serial.println("sending web page");
+  // you may have to play with this value, big pages need more porcessing time, and hence
+  // a longer timeout that 200 ms
+  server.send(200, "text/html", PAGE_MAIN);
+
+}
+
+// code to send the main web page
+// I avoid string data types at all cost hence all the char mainipulation code
+void SendXML() {
+
+  // Serial.println("sending xml");
+
+  strcpy(XML, "<?xml version = '1.0'?>\n<Data>\n");
+
+  // send bitsA0
+  sprintf(buf, "<B0>%d.%d</B0>\n", (int) (tmpSHT11), abs((int) (tmpSHT11 * 10)  - ((int) (tmpSHT11) * 10)));
+  strcat(XML, buf);
+  // send Volts0
+  sprintf(buf, "<V0>%d.%d</V0>\n", (int) (humSHT11), abs((int) (humSHT11 * 10)  - ((int) (humSHT11) * 10)));
+  strcat(XML, buf);
+
+  // send bits1
+  sprintf(buf, "<B1>%d.%d</B1>\n", (int) (tmpDS18B20), abs((int) (tmpDS18B20 * 10)  - ((int) (tmpDS18B20) * 10)));
+  strcat(XML, buf);
+  // send Volts1
+  sprintf(buf, "<V1>%d.%d</V1>\n", (int) (tmpDS18B20), abs((int) (tmpDS18B20 * 10)  - ((int) (tmpDS18B20) * 10)));
+  strcat(XML, buf);
+
+  // show led0 status
+  if (LED0) {
+    strcat(XML, "<LED>1</LED>\n");
+  }
+  else {
+    strcat(XML, "<LED>0</LED>\n");
+  }
+
+  if (SomeOutput) {
+    strcat(XML, "<SWITCH>1</SWITCH>\n");
+  }
+  else {
+    strcat(XML, "<SWITCH>0</SWITCH>\n");
+  }
+
+  strcat(XML, "</Data>\n");
+  // wanna see what the XML code looks like?
+  // actually print it to the serial monitor and use some text editor to get the size
+  // then pad and adjust char XML[2048]; above
+  Serial.println(XML);
+
+  // you may have to play with this value, big pages need more porcessing time, and hence
+  // a longer timeout that 200 ms
+  server.send(200, "text/xml", XML);
+
+
+}
+
+// I think I got this code from the wifi example
+void printWifiStatus() {
+
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("Open http://");
+  Serial.println(ip);
+}
+
+// end of code
